48154341198091d44c4e70f2f37e0fc93f0c4eed
franklinscripts <devfranklin5@gmail.com>
2023-10-16 01:05:36 +0100

    pagination fix

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -3,7 +3,7 @@ import Button from './Button'
 
 const Pagination = () => {
   return (
-    <div className='flex items-center gap-2 absolute bottom-[150px]'>
+    <div className='flex items-center gap-2 absolute md:bottom-[150px] bottom-0'>
         
         <Button text={5} className='bg-[#32D583] text-white border-none px-[20px] text-[14px] font-bold hover:opacity-80 rounded-md py-[8px]'/>
         <Button text={10} className='focus:bg-[#32D583] bg-[#F5F5F5] text-white border-none px-[20px] text-[14px] font-bold hover:opacity-80 rounded-md py-[8px]'/>
